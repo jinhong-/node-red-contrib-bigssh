51a7e82308412588dd69b66b899e410f00e25308
Jin <3448324+jinhong-@users.noreply.github.com>
2019-06-23 17:40:48 +0800

    Update bigssh.js

--- bigssh/bigssh.js
+++ bigssh/bigssh.js
@@ -104,7 +104,7 @@ module.exports = function(RED) {
 
         // Here it is, the job is starting now
         var conn = new require('ssh2').Client();
-	node.log(ssh_config);
+	this.log(ssh_config);
         // this means "biglib"
         this.working("Connecting to " + ssh_config.host + "...");
 
